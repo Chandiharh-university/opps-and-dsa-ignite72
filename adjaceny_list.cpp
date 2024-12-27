e0ad70ca55afdb1fd5f0903f455c7a379f60f7cb
ignite72 <vanshrangara925@gmail.com>
2024-12-27 10:17:07 +0530

    Update adjaceny_list.cpp

--- adjaceny_list.cpp
+++ adjaceny_list.cpp
@@ -3,13 +3,13 @@
 #include <unordered_map>
 using namespace std;
 
-// Function to add an edge between two vertices
+
 void addEdge(unordered_map<int, vector<int>>& graph, int u, int v) {
     graph[u].push_back(v); // Add v to u's list
     graph[v].push_back(u); // Add u to v's list (for undirected graph)
 }
 
-// Function to print the adjacency list
+
 void printGraph(const unordered_map<int, vector<int>>& graph) {
     for (const auto& vertex : graph) {
         cout << vertex.first << " -> ";
@@ -23,7 +23,7 @@ void printGraph(const unordered_map<int, vector<int>>& graph) {
 int main() {
     unordered_map<int, vector<int>> graph; // Graph representation using adjacency list
 
-    // Adding edges
+  
     addEdge(graph, 0, 1);
     addEdge(graph, 0, 4);
     addEdge(graph, 1, 2);
@@ -32,7 +32,7 @@ int main() {
     addEdge(graph, 2, 3);
     addEdge(graph, 3, 4);
 
-    // Printing the graph
+
     printGraph(graph);
 
     return 0;
